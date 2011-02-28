9308f432e62727c55cdfdedab397a2ec7823bee6
Egor Puzanov <epuzanov@gmx.de>
2011-02-28 19:23:21 +0100

    fix modeler plugin

--- ZenPacks/community/SQLDataSource/SQLPlugin.py
+++ ZenPacks/community/SQLDataSource/SQLPlugin.py
@@ -12,9 +12,9 @@ __doc__="""SQLPlugin
 
 wrapper for PythonPlugin
 
-$Id: SQLPlugin.py,v 1.3 2011/02/27 21:05:48 egor Exp $"""
+$Id: SQLPlugin.py,v 1.4 2011/02/28 16:28:12 egor Exp $"""
 
-__version__ = "$Revision: 1.3 $"[11:-2]
+__version__ = "$Revision: 1.4 $"[11:-2]
 
 from Products.DataCollector.plugins.CollectorPlugin import CollectorPlugin
 from Products.ZenUtils.Driver import drive
@@ -47,7 +47,8 @@ class SQLPlugin(CollectorPlugin):
             driver.next()
         try:
             if not queries:
-                queries=SQLClient().sortQueries(self.queries(device)).iteritems()
+                queries = self.queries(device)
+            queries = SQLClient().sortQueries(queries).iteritems()
             return drive(inner)
         except:
             return Failure('Syntax error in query') 
